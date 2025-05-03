57b56fa46999d0256f214cb1f929618ecc8efdc7
kaisequeira <kai.sequeira2003@gmail.com>
2025-05-03 16:17:37 +1000

    Minor linting changes

--- frontend/components/providers/theme-header.tsx
+++ frontend/components/providers/theme-header.tsx
@@ -34,7 +34,7 @@ export function ThemeHeader({ children }: ThemeHeaderProps) {
     >
       <div className="flex flex-row justify-between h-20 bg-foreground-100 shadow-md transition-colors px-5 items-center z-10">
         <div className="flex flex-row gap-3 items-center">
-          <Image alt="Rhombus AI" src="/RhombusAI.svg" width={30} height={30} />
+          <Image alt="Rhombus AI" height={30} src="/RhombusAI.svg" width={30} />
           <h1 className="tracking-tight inline font-bold from-[#0FB7CB] to-[#4986f8] bg-clip-text text-transparent text-3xl bg-gradient-to-b">
             Rhombus AI&nbsp;
           </h1>
